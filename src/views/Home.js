1548b32dc61e5175829dc0969d14201c01703ba7
vaibhavgaikwad8476 <rajnandinitikkal@gmail.com>
2023-12-10 11:59:50 +0530

    add new pages

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,12 +1,14 @@
-import React ,{ useState,useEffect } from 'react'
-import './Home.css'
-import axiox from 'axios'
+import React ,{useEffect } from 'react'
+import './Home.css';
+import axios from 'axios';
+import NewsArticle from '../component/NewsArticle/NewsArticle';
 
 export default function Home() {
     const [news , setNews] = React.useState([])
 
     const loadnews = async () =>{
-        const response = await axios.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=a24de9c871d34cbf868701ab1a6be4a7")
+        const response = await axios.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=a24de9c871d34cbf868701ab1a6be4a7'
+        );
     }
 
     useEffect(() => {
@@ -15,9 +17,18 @@ export default function Home() {
 
   return (
     <div>
-        <h1>News App</h1>
+      <h1>News App</h1>
+      <div className='news-container'>
+      {
+        news.map((newsArticle, index)=>{
+          const{author,title,description,url,urlToImage,publishedAt,content}= newsArticle
 
-
+          return(
+           <NewsArticle author={author} title={title} description={description} url={url} urlToImage={urlToImage} publishedAt={publishedAt} content={content}/>
+          )
+        })
+      }
+      </div>
     </div>
   )
 }
